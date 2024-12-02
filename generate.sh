2ad4db448f148187f483277c50b25cdc57c0dad3
wangyanhui666 <wangyanhui@mail.ustc.edu.cn>
2024-12-02 08:29:48 +0000

    add generate script

---
+++ generate.sh
@@ -0,0 +1,13 @@
+torchrun --nnodes=1 --nproc_per_node=4 generate.py \
+  --model SiT-XL/2 \
+  --num-fid-samples 50000 \
+  --ckpt /home/t2vg-a100-G4-40/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints/0250000.pt \
+  --sample-dir  /home/t2vg-a100-G4-40/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0250000\
+  --path-type=linear \
+  --encoder-depth=8 \
+  --projector-embed-dims=768 \
+  --per-proc-batch-size=64 \
+  --mode=sde \
+  --num-steps=250 \
+  --cfg-scale=1.0 \
+  --guidance-high=0.7
