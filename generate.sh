5b6cc1a20b93d0312eda681f6449922ca6010b34
wangyanhui666 <wangyanhui@mail.ustc.edu.cn>
2024-12-02 08:34:29 +0000

    fix double dir bug

--- generate.sh
+++ generate.sh
@@ -1,8 +1,9 @@
 torchrun --nnodes=1 --nproc_per_node=4 generate.py \
   --model SiT-XL/2 \
   --num-fid-samples 50000 \
-  --ckpt /home/t2vg-a100-G4-40/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints/0250000.pt \
-  --sample-dir  /home/t2vg-a100-G4-40/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0250000\
+  --ckpt /home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints/0250000.pt \
+  --sample-dir  /home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0250000\
+  --vae "mse" \
   --path-type=linear \
   --encoder-depth=8 \
   --projector-embed-dims=768 \
--- run_sing_repa_multinode.sh
+++ run_sing_repa_multinode.sh
@@ -33,6 +33,6 @@ accelerate launch \
     --encoder-depth=8 \
     --max-train-steps 500000 \
     --num-workers ${1} \
-    --output-dir /mnt/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1 \
+    --output-dir /mnt/t2vg/dit/logs/train_repa_256/ \
     --exp-name="1201_linear-dinov2-b-enc8_baseline_1" \
     --data-dir="/mnt/t2vgusw2_videos/imagenet/sd_latents/REPA_256"
