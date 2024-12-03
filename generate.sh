e22a490231c052357a2cfc9f48322a6bc2d8ff3b
wangyanhui666 <wangyanhui@mail.ustc.edu.cn>
2024-12-03 05:12:55 +0000

    add generate.sh

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
 torchrun --nnodes=1 --nproc_per_node=4 generate.py \
   --model SiT-XL/2 \
   --num-fid-samples 50000 \
-  --ckpt /home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints/0250000.pt \
-  --sample-dir  /home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0250000\
-  --vae "mse" \
+  --ckpt /home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints/0400000.pt \
+  --sample-dir  /home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0400000\
+  --vae "ema" \
   --path-type=linear \
   --encoder-depth=8 \
   --projector-embed-dims=768 \
@@ -13,6 +13,6 @@ torchrun --nnodes=1 --nproc_per_node=4 generate.py \
   --cfg-scale=1.0 \
   --guidance-high=0.7 && \
 
-  echo "Done"
+echo "Done"
 
-# python create_npz_from_sample_folder.py --sample_folder_dir "/home/t2vg-a100-G4-40/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0250000/SiT-XL-2-0250000-size-256-vae-ema-cfg-1.0-seed-0-sde/"
+# python create_npz_from_sample_folder.py --sample_folder_dir "/home/t2vg-a100-G4-42/guangtingsc/t2vg/dit/logs/train_repa_256/1201_linear-dinov2-b-enc8_baseline_1/1201_linear-dinov2-b-enc8_baseline_1/checkpoints_0250000/SiT-XL-2-0250000-size-256-vae-mse-cfg-1.0-seed-0-sde/"
